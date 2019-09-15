584ce455e7a6807ec284246b2da432cb07b9d473
Seung Wook Lee <seungwooklee94@gmail.com>
2019-09-15 11:55:57 +0900

    Tested Mutation. Now let's try some APIs

--- graphql/db.js
+++ graphql/db.js
@@ -1,30 +1,61 @@
-export const people = [
+// const is read-only. So defining my DB w/ 'let'
+let movies = [
+  {
+    id: "0",
+    name: "Avengers - End game",
+    score: 4
+  },
   {
     id: "1",
-    name: "Seung Wook Lee",
-    age: 25,
-    gender: "male"
+    name: "The Godfather",
+    score: 5
   },
   {
     id: "2",
-    name: "Minas Tirith",
-    age: 1111,
-    gender: "female"
+    name: "Pulp Fiction",
+    score: 5
   },
-  { id: "3", name: "Kodo domo the third", age: 48, gender: "male" },
-  { id: "4", name: "Kowa mina", age: 18, gender: "female" },
+  {
+    id: "3",
+    name: "The Good, the Bad and the Ugly",
+    score: 4
+  },
+  {
+    id: "4",
+    name: "The Lord of the Rings: The Return of the King",
+    score: 4
+  },
   {
     id: "5",
-    name: "Elessar, the chosen King of Gondor",
-    age: 98,
-    gender: "male"
-  },
-  { id: "6", name: "Arwen, light of the evenstar", age: 122, gender: "female" },
-  { id: "7", name: "Sauron, the dark lord", age: 1834, gender: "male" }
+    name: "12 Angry Men",
+    score: 3
+  }
 ];
 
 // Filter: go through all conditions and spit out what matches with the defined condition
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id) );
-  return filteredPeople[0]
+  const filteredMovies = movies.filter(movie => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+export const getMovies = () => movies;
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    name,
+    score,
+    id: `${movies.length}`
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,21 @@
-import { people, getById } from "./db";
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 /*
 * In resolvers, you create functions that resolves operation
 And you can have any array of DBs.
-? But isn't GraphQL a database? So you need to have a physical file of DB to load? Hmm!
+* The point is you can hook up any kind of backend you'd like. And 쪼물쪼물 
+클라이언트-GraphQL 간 comm이 있고 GraphQL - API (ex. REST API) 간의 통신이 있다.
 */
 
 const resolvers = {
   // people return people as an array
   Query: {
-    people: () => people,
-    person: (_obj_, { id }) => getById(id)
+    movies: () => getMovies(),
+    movie: (_obj_, { id }) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, {id}) => deleteMovie(id)
   }
 };
 
